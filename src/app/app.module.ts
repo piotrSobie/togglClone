cb4b1bbd308f237b4470e0ee212f8d35d8176aff
piotrSobie <piotrsobiegraj736@gmail.com>
2019-08-01 18:19:05 +0200

    fix(app.module.ts, shared.module.ts): fix dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import {AppRoutingModule} from "./app-routing.module";
@@ -9,18 +8,15 @@ import { TrainingComponent } from './training/training.component';
 import { PricingComponent } from './pricing/pricing.component';
 import { FeaturesComponent } from './features/features.component';
 import {SharedModule} from "./shared/shared.module";
-import { FooterComponent } from './shared/footer/footer.component';
 import {PasswordValidatorDirective} from "./auth/password-validator.directive";
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     LoginComponent,
     RegisterComponent,
     TrainingComponent,
     PricingComponent,
     FeaturesComponent,
-    FooterComponent,
     PasswordValidatorDirective
   ],
   imports: [
